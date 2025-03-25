754c1a8e8a50864c3058468067e0d941d747bb01
tiwaris1 <shivang.tiwari@autodesk.com>
2025-03-25 15:12:17 +0530

    comments resolved

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { StoreProvider } from "easy-peasy";
 import Store from "./store/store.ts";
+import InitialLoading from "./web/commonComponent/initialLoading/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <StoreProvider store={Store}>
-      <App />
+      <InitialLoading />
     </StoreProvider>
   </StrictMode>
 );
